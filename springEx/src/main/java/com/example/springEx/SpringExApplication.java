12ee0041cc4d0faa931d624a0b45d3aef9adbda8
moonjinho99 <anszl789@naver.com>
2024-01-20 15:18:03 +0900

    List테스트(스프링)

--- springEx/src/main/java/com/example/springEx/SpringExApplication.java
+++ springEx/src/main/java/com/example/springEx/SpringExApplication.java
@@ -9,5 +9,4 @@ public class SpringExApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringExApplication.class, args);
 	}
-
 }
--- springEx/src/main/java/com/example/springEx/controller/JoinController.java
+++ springEx/src/main/java/com/example/springEx/controller/JoinController.java
@@ -1,10 +1,15 @@
 package com.example.springEx.controller;
 
+import com.example.springEx.dto.ListItem;
 import com.example.springEx.dto.User;
 import lombok.extern.java.Log;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
+
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Slf4j
@@ -21,6 +26,9 @@ public class JoinController {
     @ResponseBody
     public String saveUser(@RequestBody User user){
 
+        System.out.println("이름 : "+user.getUsername());
+        System.out.println("나이 : "+user.getAge());
+
         if(user.getUsername().equals("mmm") && user.getAge() == 25)
         {
             return "success";
@@ -29,6 +37,43 @@ public class JoinController {
         else{
             return "fail";
         }
-
     }
+
+
+    @GetMapping("/test")
+    public String mainPage()
+    {
+        return "/test/test";
+    }
+
+
+    @PostMapping(value = "/finduser", produces = "application/json;charset=utf-8")
+    @ResponseBody
+    public List<User> findUser(@RequestBody String username){
+
+        User user = new User("aaa",22);
+
+        List<User> userList = new ArrayList<>();
+        userList.add(user);
+
+        return userList;
+    }
+
+
+    @GetMapping(value="/find_list" , produces = "application/json;charset=utf-8")
+    @ResponseBody
+    public List<ListItem> findList(){
+
+        System.out.println("통신 성공");
+        ListItem list;
+        List<ListItem> listItems = new ArrayList<>();
+        for(int i=0; i<10; i++)
+        {
+            list = new ListItem("a"+i,"200"+i);
+            listItems.add(list);
+        }
+       return listItems;
+    }
+
+
 }
